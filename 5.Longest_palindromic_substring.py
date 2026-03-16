867b6586fadd1a34c72d28fd099bca4584aa3396
J Dipayan Rao <raodipayan@gmail.com>
2026-03-16 23:47:37 +0530

    15-03-2026

---
+++ 5.Longest_palindromic_substring.py
@@ -0,0 +1,40 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        start=0
+        maxlen=0
+        def expand(left,right):
+            while left>=0 and right<len(s) and s[left]==s[right]:
+                left-=1
+                right+=1
+            return right-left-1
+
+        for i in range(len(s)):
+            len1=expand(i,i) #odd palindrome
+            len2=expand(i,i+1) #even palindrome
+            length=max(len1,len2)
+            if length>maxlen:
+                maxlen=length
+                start=i-(length-1)//2
+        return s[start:start+maxlen]
+    
+"""
+Given a string s, return the longest palindromic substring in s.
+
+ 
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consist of only digits and English letters.
+"""
